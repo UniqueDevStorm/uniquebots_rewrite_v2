091abec134d82c450a36589ed188166e46589870
UniqueDevStorm <storm@uniquecode.team>
2021-02-28 21:44:24 +0900

    feat: addbot

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -6,23 +6,29 @@ class Layout extends Component {
         return (
             <div>
                 {/*nav area*/}
-                <nav className="navbar py-3 navbar-expand-lg navbar-light bg-light fw-bolder">
-                    <div className="container-fluid">
-                        <div className="container">
+                <div className="bg-light fixed-top">
+                    <nav className="navbar py-3 navbar-expand-lg navbar-light fw-bolder container">
+                        <div className="container-fluid">
                             <Link href="/">
                                 <a className="navbar-brand" href="/">UNIQUEBOTS</a>
                             </Link>
-                            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#layout__navbarContent" aria-controls="layout__navbarContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
+                                    data-bs-target="#layout__navbarContent" aria-controls="layout__navbarContent"
+                                    aria-expanded="false" aria-label="Toggle navigation">
                                 <span className="navbar-toggler-icon"/>
                             </button>
                             <div className="collapse navbar-collapse" id="layout__navbarContent">
-                                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-
+                                <ul className="navbar-nav me-auto">
+                                    <li className="nav-item">
+                                        <Link href="/addbot">
+                                            <a className="nav-link" href="/addbot">봇 추가하기</a>
+                                        </Link>
+                                    </li>
                                 </ul>
                             </div>
                         </div>
-                    </div>
-                </nav>
+                    </nav>
+                </div>
                 {this.props.children}
             </div>
         );
