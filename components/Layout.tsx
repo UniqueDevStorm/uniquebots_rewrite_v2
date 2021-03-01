a251a5592ae929f2d79d17a3d3cfe9d41dcd1fd1
pikokr <parangee9706@gmail.com>
2021-03-01 15:35:15 +0900

    feat: add generic on layout

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import Link from 'next/link'
 
-class Layout extends Component {
+class Layout extends Component<any, any> {
     render() {
         return (
             <div>
